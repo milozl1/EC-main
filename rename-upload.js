2f6c3a242a6431821d0af12a8bedea4be532eb92
milozl1 <zlatan.milosevic@forvia.com>
2026-01-26 08:45:32 +0200

    Change date format from 'YYMMDD' to 'YY.MM.DD'

--- rename-upload.js
+++ rename-upload.js
@@ -42,7 +42,7 @@
     const dd = String(d.getDate()).padStart(2, '0');
     const mm = String(d.getMonth() + 1).padStart(2, '0');
     const yy = String(d.getFullYear()).slice(-2);
-    return `${yy}${mm}${dd}`;
+    return `${yy}.${mm}.${dd}`;
   }
 
   /**
